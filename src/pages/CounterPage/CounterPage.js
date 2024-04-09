b0c750bf98fa025ef2d2680bc0335b7e102ad22e
RomanasV <romanasvenckus@gmail.com>
2024-04-09 14:43:27 +0300

    add grade button added

--- src/pages/CounterPage/CounterPage.js
+++ src/pages/CounterPage/CounterPage.js
@@ -4,30 +4,7 @@ const CounterPage = () => {
   const INITIAL_NUM = 5
 
   const [num, setNum] = useState(INITIAL_NUM)
-
-  const checkButtonDisable = (count) => {
-    if (count > 0 && num > 10 - count) {
-      return true
-    }
-
-    if (count < 0 && num <= Math.abs(count)) {
-      return true
-    }
-
-    return false
-  }
-
-  const buttonNums = [5, 2, 1, -1, -2, -5]
-
-  const buttonsList = buttonNums.map((buttonNum, index) => (
-    <button 
-      key={index} 
-      onClick={() => numHandler(buttonNum)} 
-      disabled={checkButtonDisable(buttonNum)}
-    >
-      {buttonNum}
-    </button>
-  ))
+  const [grades, setGrades] = useState([])
 
   const numHandler = count => {
     // setNum(num + count)
@@ -41,16 +18,53 @@ const CounterPage = () => {
   
   const inputNumHandler = event => setNum(Number(event.target.value))
 
-  const displayColor = num >= 5 ? 'green' : 'red'
+  const addGradeHandler = () => {
+    // setGrades(prevState => {
+    //   // const newState = [...prevState]
+    //   // newState.unshift(num)
+
+    //   const newState = [num, ...prevState]
+    //   return newState
+    // })
+
+    setGrades(prevState => [num, ...prevState])
+  }
+
+  // const getColor = grade => {
+  //   if (grade >= 5) {
+  //     return 'green'
+  //   } else {
+  //     return 'red'
+  //   }
+  // }
+
+  const getColor = grade => grade >= 5 ? 'green' : 'red'
 
   return (
     <div>
       <input type="number" min="1" max="10" value={num} onChange={inputNumHandler} />
-      <h3 style={{color: displayColor}}>{num}</h3>
+      <h3 style={{color: getColor(num)}}>{num}</h3>
 
-      {buttonsList}
+      <button onClick={() => numHandler(5)} disabled={num > 5}>+5</button>
+      <button onClick={() => numHandler(2)} disabled={num > 8}>+2</button>
+      <button onClick={() => numHandler(1)} disabled={num > 9}>+1</button>
+      <button onClick={() => numHandler(-1)} disabled={num <= 1}>-1</button>
+      <button onClick={() => numHandler(-2)} disabled={num <= 2}>-2</button>
+      <button onClick={() => numHandler(-5)} disabled={num <= 5}>-5</button>
       
       <button onClick={() => setNum(INITIAL_NUM)}>Reset</button>
+
+      <button onClick={addGradeHandler}>Add Grade</button>
+
+      <div>
+        <h4>{grades.length > 0 ? 'Grades:' : 'No grades yet... Please add a grade.'}</h4>
+
+        {grades.length > 0 && (
+          <ul>
+            {grades.map((grade, index) => <li style={{color: getColor(grade)}} key={index}>{grade}</li>)}
+          </ul>
+        )}
+      </div>
     </div>
   )
 }
