a5ed06b4de55ba3e3e988d974dd8093b25f1e717
RomanasV <romanasvenckus@gmail.com>
2024-04-09 11:54:36 +0300

    prevState added

--- src/pages/CounterPage/CounterPage.js
+++ src/pages/CounterPage/CounterPage.js
@@ -1,26 +1,35 @@
 import { useState } from "react"
 
 const CounterPage = () => {
-  const [num, setNum] = useState(5)
+  const INITIAL_NUM = 5
 
-  const plus5Handler = () => setNum(num + 5)
-  const plus2Handler = () => setNum(num + 2)
-  const plus1Handler = () => setNum(num + 1)
-  const minus1Handler = () => setNum(num - 1)
-  const minus2Handler = () => setNum(num - 2)
-  const minus5Handler = () => setNum(num - 5)
+  const [num, setNum] = useState(INITIAL_NUM)
+
+  const numHandler = count => {
+    // setNum(num + count)
+
+    // setNum(prevState => {
+    //   return prevState + count
+    // })
+
+    setNum(prevState => prevState + count)
+  }
+  
+  const inputNumHandler = event => setNum(Number(event.target.value))
 
   const displayColor = num >= 5 ? 'green' : 'red'
 
   return (
     <div>
+      <input type="number" min="1" max="10" value={num} onChange={inputNumHandler} />
       <h3 style={{color: displayColor}}>{num}</h3>
-      <button onClick={plus5Handler} disabled={num > 5}>+5</button>
-      <button onClick={plus2Handler} disabled={num > 8}>+2</button>
-      <button onClick={plus1Handler} disabled={num > 9}>+1</button>
-      <button onClick={minus1Handler} disabled={num <= 1}>-1</button>
-      <button onClick={minus2Handler} disabled={num <= 2}>-2</button>
-      <button onClick={minus5Handler} disabled={num <= 5}>-5</button>
+      <button onClick={() => numHandler(5)} disabled={num > 5}>+5</button>
+      <button onClick={() => numHandler(2)} disabled={num > 8}>+2</button>
+      <button onClick={() => numHandler(1)} disabled={num > 9}>+1</button>
+      <button onClick={() => numHandler(-1)} disabled={num <= 1}>-1</button>
+      <button onClick={() => numHandler(-2)} disabled={num <= 2}>-2</button>
+      <button onClick={() => numHandler(-5)} disabled={num <= 5}>-5</button>
+      <button onClick={() => setNum(INITIAL_NUM)}>Reset</button>
     </div>
   )
 }
